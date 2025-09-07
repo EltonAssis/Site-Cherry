e14d838d60d7dbba8e7e998b510ae7c4b895de4d
EltonAssis <eltonooficial@gmail.com>
2025-09-07 16:24:37 +0100

    alterações

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-var MenuItens = document.getElementsById
+var MenuItens = document.getElementById("MenuItens");
+MenuItens.style.maxHeight = "100px";
+
+function menucelular(){
+    if(MenuItens.style.maxHeight == "0px"){
+        MenuItens.style.maxHeight = "200px";
+    }else{
+        MenuItens.style.maxHeight = "0px";
+    }
+}
